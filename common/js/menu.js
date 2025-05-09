5af2e667c4fb0577a5e176007ceea3e70997b457
shin <finetonight@gmail.com>
2025-05-09 10:08:14 +0900

    first commit

---
+++ common/js/menu.js
@@ -0,0 +1,28 @@
+var MENU = [
+  {
+    id: "0",
+    title: "Group",
+    anchor: "#n",
+    active: false,
+    sub: [
+      {
+        id: "0-1",
+        title: "We are Misto",
+        anchor: "#n",
+        active: false,
+      },
+      {
+        id: "0-2",
+        title: "연혁",
+        anchor: "#n",
+        active: false,
+      },
+      {
+        id: "0-3",
+        title: "브랜드",
+        anchor: "#n",
+        active: false,
+      },
+    ],
+  },
+];
